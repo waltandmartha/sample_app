6164692a75bfdf78fc957190fc02cedde8f59afd
Glenn <waltandmartha@gmail.com>
2019-02-03 23:08:01 +1100

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -33,5 +33,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    # assert_equal XXXX, assigns(:user).XXXX
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie
+    log_in_as(@user, remember_me: "1")
+    # Log in again and verify that cookie is deleted
+    log_in_as(@user, remember_me: "0")
+    assert_empty cookies[:remember_token] 
   end 
 end
